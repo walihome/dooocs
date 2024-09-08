72a7546096e7a17a823b818ce71f7d2994b4ba37
logan <szqsunny@outlook.com>
2024-09-08 12:56:18 +0800

    修改config配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
     sidebar: sidebar,
 
     editLink: {
-      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+      pattern: 'https://github.com/walihome/dooocs/tree/main/docs/:path',
       text: 'Edit this page on GitHub'
     },
     footer: {
