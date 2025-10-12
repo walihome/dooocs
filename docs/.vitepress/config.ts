09297a2906256d9db78e1e2d44c362664d0c65d5
logan <szqsunny@outlook.com>
2025-10-12 19:37:48 +0800

    提交需求 -> 聊一聊

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
     logo: '/logo.png',
     nav: [
       { text: 'Home', link: '/' },
-      { text: '提交需求', link: 'https://chatbot.weixin.qq.com/webapp/oqZiTntQCQC6wbCGGCKWod8KybXWNF?robotName=%E5%B0%8F%E7%BD%97' }
+      { text: '聊一聊', link: 'https://chatbot.weixin.qq.com/webapp/oqZiTntQCQC6wbCGGCKWod8KybXWNF?robotName=%E5%B0%8F%E7%BD%97' }
     ],
     lastUpdated: {
       text: 'Updated at',
